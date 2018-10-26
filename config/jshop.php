f99d1204c90b58a017b226ed12f604e14ee5a243
wjima <1113160783@qq.com>
2018-10-26 16:32:04 +0800

    更新 jshop.php

--- config/jshop.php
+++ config/jshop.php
@@ -12,7 +12,7 @@ return [
     //商品导入模板
     'goods_import_templete' => ROOT_PATH . 'public' . DS . 'static' . DS . 'templete' .DS. 'goods-csv-import.csv',
     //快递查询配置参数
-    'api_express_key' => 'e5572466f046924e506ebf43dbccc786',
+    'api_express_key' => 'e5572466f046924e506ebf43dbc',
 
     'login_fail_num' => 3,              //登陆失败次数，如果每天登陆失败次数超过次数字，就会显示图片验证码
     'tocash_money_low' => '100' ,       //最低提现金额

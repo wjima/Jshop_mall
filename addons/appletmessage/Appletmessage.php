e8a4c82ab3c17b28d8e19247465407b72dd0ed8b
mark <1113160783@qq.com>
2018-11-02 14:15:08 +0800

    消息提醒

--- addons/appletmessage/Appletmessage.php
+++ addons/appletmessage/Appletmessage.php
@@ -2,9 +2,11 @@
 
 namespace addons\appletmessage;    // 注意命名空间规范
 
+use app\common\model\TemplateMessage;
 use app\common\model\WeixinAuthor;
 use myxland\addons\Addons;
 use app\common\model\Addons as addonsModel;
+use org\Wx;
 
 /**
  * 微信小程序模板消息插件
@@ -68,32 +70,46 @@ class Appletmessage extends Addons
         if (!$params['params']['user_id']) {
             return false;
         }
-        $addonModel        = new addonsModel();
-        $addon             = $addonModel->getAddonByName($this->info['name']);
-        $setting           = $addonModel->getSetting($this->info['name']);
-        if(!$setting){
+        $addonModel = new addonsModel();
+        $setting    = $addonModel->getSetting($this->info['name']);
+
+        if (!$setting) {
             return false;
         }
-        $template          = $setting[$params['params']['code']];
+        $template = $setting[$params['params']['code']];
+
         //发送消息，取出会员open_id，然后发送
         $wxUserinfo = getUserWxInfo($params['params']['user_id']);
         if (!$wxUserinfo) {
             return false;
         }
-        $authorModel = new WeixinAuthor();
-        $appinfo     = $authorModel->getAuthorInfo('b2c');
-        if (!$appinfo) {
-            return false;
+        $templateMessageModel = new TemplateMessage();
+
+        if ($params['params']['code'] == 'create_order') {
+            $id                                   = $params['params']['params']['order_id'];
+            $closeOrder                           = getSetting('order_cancel_time') * 24;
+            $params['params']['params']['notice'] = '您的订单将在' . $closeOrder . '小时候取消，请及时付款哦';
+            $formInfo                             = $templateMessageModel->where(['type' => $params['params']['code'], 'code' => $id, 'status' => '1'])->find();
+
         }
+        if ($params['params']['code'] == 'delivery_notice') {
+            $formInfo = $templateMessageModel->where(['type' => 'order_payed', 'code' => $id, 'status' => '1'])->find();
+        }
+        $appid       = getSetting('wx_appid');
+        $secret      = getSetting('wx_app_secret');
         $template_id = $template['template_id'];
         unset($template['template_id']);
-        $message['data']        = $this->replaceWord($params['params']['params'], $template);
-        $message['touser']      = $wxUserinfo['openid'];
-        $message['template_id'] = $template_id;
-        $message['url']         = '';
-        $message['form_id']     = md5(time());//formid
-        $wechat = &load_wechat('Message', $appinfo['appid']);
-        $wechat->sendTemplateMessage($message);
+        $params['params']['params']['seller_name'] = getSetting('shop_name');
+        $message['data']                           = $this->replaceWord($params['params']['params'], $template);
+        $message['touser']                         = $wxUserinfo['openid'];
+        $message['template_id']                    = $template_id;
+        $message['page']                           = 'index/index';
+        $message['form_id']                        = $formInfo['from_id'];//formid
+        $wx                                        = new Wx();
+        $res                                       = $wx->sendTemplateMessage($appid, $secret, $message);
+        if ($res) {
+            $templateMessageModel->sendSuccess($formInfo['id']);
+        }
     }
 
     /**

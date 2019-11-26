4a254e36cf089e3e3c7de7495620b376f0313a19
keinx <echo@keinx.com>
2019-11-26 09:23:15 +0800

    '修复发货的商品addon为null的时候的问题'

--- application/common/model/BillDelivery.php
+++ application/common/model/BillDelivery.php
@@ -129,7 +129,7 @@ class BillDelivery extends Common
                 'sn' => $dinfo['items'][$product_id]['sn'],
                 'weight' => $dinfo['items'][$product_id]['weight'],
                 'name' => $dinfo['items'][$product_id]['name'],
-                'addon' => $dinfo['items'][$product_id]['addon'],
+                'addon' => $dinfo['items'][$product_id]['addon'] ? $dinfo['items'][$product_id]['addon'] : '',
                 'nums' => $num
             ];
         }

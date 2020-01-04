ad529a2d025604833bc3cddd37e1cfa890c4b42c
wjima <1113160783@qq.com>
2020-01-04 15:01:50 +0800

    修改表前缀

--- word/2019-12-06-发货明细addons字段设置默认值.sql
+++ word/2019-12-06-发货明细addons字段设置默认值.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `bill_delivery_items` CHANGE `addon` `addon` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;
+ALTER TABLE `jshop_bill_delivery_items` CHANGE `addon` `addon` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;

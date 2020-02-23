21be10b782ce457dee609ef6e37b49ac419452ba
wjima <1113160783@qq.com>
2020-02-23 18:37:20 +0800

    商品增加默认类型

---
+++ word/2020-02-23-商品增加默认类型.sql
@@ -0,0 +1,82 @@
+
+-- ----------------------------
+-- Records of jshop_goods_params
+-- ----------------------------
+INSERT INTO `jshop_goods_params` VALUES (1, '产地', '', 'text', 1582454016, 1582454016);
+INSERT INTO `jshop_goods_params` VALUES (2, '材质', '', 'text', 1582454017, 1582454017);
+INSERT INTO `jshop_goods_params` VALUES (3, '型号', '', 'text', 1582454017, 1582454017);
+
+
+-- ----------------------------
+-- Records of jshop_goods_type
+-- ----------------------------
+INSERT INTO `jshop_goods_type` VALUES (3, '通用类型', NULL);
+
+
+
+-- ----------------------------
+-- Records of jshop_goods_type_params
+-- ----------------------------
+INSERT INTO `jshop_goods_type_params` VALUES (1, 3);
+INSERT INTO `jshop_goods_type_params` VALUES (2, 3);
+INSERT INTO `jshop_goods_type_params` VALUES (3, 3);
+
+
+
+-- ----------------------------
+-- Records of jshop_goods_type_spec
+-- ----------------------------
+INSERT INTO `jshop_goods_type_spec` VALUES (3, '规格', 100);
+
+
+
+-- ----------------------------
+-- Records of jshop_goods_type_spec_rel
+-- ----------------------------
+INSERT INTO `jshop_goods_type_spec_rel` VALUES (3, 3);
+
+
+
+-- ----------------------------
+-- Records of jshop_goods_type_spec_value
+-- ----------------------------
+INSERT INTO `jshop_goods_type_spec_value` VALUES (32, 3, '规格1', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (33, 3, '规格2', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (34, 3, '规格3', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (35, 3, '规格4', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (36, 3, '规格5', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (37, 3, '规格6', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (38, 3, '规格7', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (39, 3, '规格8', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (40, 3, '规格9', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (41, 3, '规格10', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (42, 3, '规格11', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (43, 3, '规格12', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (44, 3, '规格13', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (45, 3, '规格14', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (46, 3, '规格15', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (47, 3, '规格16', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (48, 3, '规格17', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (49, 3, '规格18', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (50, 3, '规格19', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (51, 3, '规格20', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (52, 3, '规格21', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (53, 3, '规格22', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (54, 3, '规格23', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (55, 3, '规格24', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (56, 3, '规格25', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (57, 3, '规格26', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (58, 3, '规格27', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (59, 3, '规格28', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (60, 3, '规格29', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (61, 3, '规格30', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (62, 3, '规格31', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (63, 3, '规格32', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (64, 3, '规格33', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (65, 3, '规格34', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (66, 3, '规格35', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (67, 3, '规格36', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (68, 3, '规格37', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (69, 3, '规格38', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (70, 3, '规格39', 100);
+INSERT INTO `jshop_goods_type_spec_value` VALUES (71, 3, '规格40', 100);

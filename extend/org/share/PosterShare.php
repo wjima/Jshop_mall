7a3fdf41d2480db3b7d25272aa2dc3d5c190e09a
曾经最美 <hnhaitao@163.com>
2020-03-09 15:38:53 +0800

    share bug

--- extend/org/share/PosterShare.php
+++ extend/org/share/PosterShare.php
@@ -122,9 +122,9 @@ class PosterShare extends QrShare implements BaseShare
         if(!isset($this->c['page_'.$page])){
             return $this->getQr($url, $re['data']['code'],$client);
         }
-        if($client != self::CLIENT_WXMNAPP){
-            $url = $this->getUrl($url,$re['data']['code']);
-        }
+//        if($client != self::CLIENT_WXMNAPP){
+//            $url = $this->getUrl($url,$re['data']['code']);
+//        }
         $url = urlencode($url);
         $re['data'] = url('b2c/common/poster',['client' => $client,'code' => $re['data']['code']],true,true)."?url=".$url;
         return $re;

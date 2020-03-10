15ddb5f97b4ee7a0cdc48f030173b5bfb0e4689f
wjima <1113160783@qq.com>
2020-03-10 14:11:48 +0800

    后台公众号提示优化，前台增加版权标示

--- uni-app/components/jihai-copyright/jihaiCopyright.vue
+++ uni-app/components/jihai-copyright/jihaiCopyright.vue
@@ -1,15 +1,22 @@
 <template>
 	<view class="cpr">
 		<view class="color-9">
-			吉海科技 © jihainet.com
+			吉海科技 © jihainet.com 版权所有
 		</view>
-		<view class="color-9">
-			版权所有
+		<view class="color-9 beian">
+			<view v-if="shop_beian"><a href="http://beian.miit.gov.cn/" target="_blank">备案号：{{shop_beian}}</a></view>
 		</view>
 	</view>
 </template>
 
 <script>
+export default {
+	data() {
+		return {
+			shop_beian: this.$store.state.config.shop_beian||''
+		}
+	}
+}
 </script>
 
 <style>
@@ -17,5 +24,9 @@
 	text-align: center;
 	font-size: 24upx;
 	margin: 20upx 0;
+}
+.beian a{
+	text-decoration: none;
+	color: #999 !important;
 }
 </style>

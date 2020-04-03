eda11d390e86c5b0976bf54dffd33ffd933e0786
jingyi <1270411918@qq.com>
2020-04-03 18:50:00 +0800

    修复优惠券问题

--- uni-app/pages/goods/place-order/index.vue
+++ uni-app/pages/goods/place-order/index.vue
@@ -1,11 +1,12 @@
 <template>
 	<form class="content" @submit="toPay" report-submit="true">
 		<view class="content-top">
-			<uni-segmented-control :current="type_current" :values="type_items" @clickItem="onTypeItem" style-type="text" active-color="#333" v-if="storeSwitch == 1"></uni-segmented-control>
+			<uni-segmented-control :current="type_current" :values="type_items" @clickItem="onTypeItem" style-type="text"
+			 active-color="#333" v-if="storeSwitch == 1"></uni-segmented-control>
 			<view class="content">
 				<view v-show="type_current === 0">
 					<!-- 收货地址信息 -->
-					<view class='cell-group margin-cell-group' v-if="userShip && userShip.id"  @click="showAddressList">
+					<view class='cell-group margin-cell-group' v-if="userShip && userShip.id" @click="showAddressList">
 						<view class='cell-item add-title-item right-img cell-item-mid'>
 							<view class='cell-item-hd'>
 								<image class='cell-hd-icon' src='/static/image/location.png'></image>
@@ -70,7 +71,7 @@
 					<view class='cell-item-hd'>
 						<view class='cell-hd-title'>电话</view>
 					</view>
-					<view class='cell-item-bd'> 
+					<view class='cell-item-bd'>
 						<input class='cell-bd-input' placeholder='请输入提货人电话' v-model="store_pick.mobile" style="width: 100%;"></input>
 					</view>
 				</view>
@@ -86,7 +87,8 @@
 							<view class='goods-price'>￥{{ item.products.price || ''}}</view>
 						</view>
 						<view class="romotion-tip" v-if="item.products.promotion_list">
-							<view class="romotion-tip-item" :class="v.type !== 2 ? 'bg-gray' : ''" v-for="(v, k) in item.products.promotion_list" :key="k">
+							<view class="romotion-tip-item" :class="v.type !== 2 ? 'bg-gray' : ''" v-for="(v, k) in item.products.promotion_list"
+							 :key="k">
 								{{ v.name || ''}}
 							</view>
 						</view>
@@ -117,12 +119,13 @@
 							积分抵扣
 						</view>
 						<view class="cell-bd-view">
-							<text class="cell-bd-text address color-9">可用 {{ canUsePoint || ''}} 积分，可抵扣 {{ pointMoney || ''}} 元，共有 {{ userPointNums|| '' }} 积分。</text>
+							<text class="cell-bd-text address color-9">可用 {{ canUsePoint || ''}} 积分，可抵扣 {{ pointMoney || ''}} 元，共有 {{ userPointNums|| '' }}
+								积分。</text>
 						</view>
 					</view>
 					<view class='cell-item-ft' @click="changePointHandle">
 						<label class="radio">
-							<radio value="1" :checked="isUsePoint" color="#FF7159"/>
+							<radio value="1" :checked="isUsePoint" color="#FF7159" />
 							<!-- <radio class="radioNo" disabled="true"/> -->
 						</label>
 					</view>
@@ -133,7 +136,7 @@
 						<view class='cell-hd-title'>发票</view>
 					</view>
 					<view class='cell-item-ft' @click="goInvoice()">
-						
+
 						<text class='cell-ft-text'>{{invoice.name|| ''}}</text>
 						<image class='cell-ft-next icon' src='/static/image/right.png'></image>
 					</view>
@@ -167,10 +170,10 @@
 				</view>
 				<view class="cell-textarea ">
 					<!-- #ifdef MP-WEIXIN -->
-					<input v-model="memo" placeholder="50字以内(选填)"/>
+					<input v-model="memo" placeholder="50字以内(选填)" />
 					<!-- #endif -->
 					<!-- #ifndef MP-WEIXIN -->
-					<textarea v-model="memo" placeholder="50字以内(选填)" maxlength="50"/>
+					<textarea v-model="memo" placeholder="50字以内(选填)" maxlength="50" />
 					<!-- #endif -->
 				</view>
 			</view>
@@ -252,21 +255,38 @@
 						</view>
 
 						<view class="coupon-c" v-show="current === 1">
+							<scroll-view class="coupon-c" scroll-y="true">
 							<view class="coupon-enter">
 								<view class="coupon-input">
 									<input type="text" v-model="inputCouponCode" placeholder="请输入优惠券码"/>
 								</view>
+								
 								<!-- #ifdef MP-WEIXIN -->
 								<!-- <view class="coupon-code">
 									<image src="/static/image/ewm.png" class="icon" mode="" @click="scanCode"></image>
 								</view> -->
 								<!-- #endif -->
+								
 								<view class="coupon-enter-btn"
 									  @click="useInputCouponCode"
 								>
 									<button class="btn btn-b">确认</button>
 								</view>
 							</view>
+								<view class="coupon-list" v-for="(item,index) in inputCouArr" :key="index">
+									<view class="coupon-item">
+										<view class="color-6 fsz28 coupon-name">
+											{{item.name}}
+										</view>
+										<view class="color-9 fsz24 coupon-code1">
+											{{item.code}}
+										</view>
+										<view class="delete" @click="removeCouponCode(item.code)">
+											<image src="/static/image/del.png" mode=""></image>
+										</view>
+									</view>
+								</view>
+							</scroll-view>
 						</view>
 					</view>
 					<view class="button-bottom">
@@ -346,7 +366,8 @@
                     mobile: ''
                 },
                 team_id: 0, //拼团id
-				submitStatus: false
+				submitStatus: false,
+				inputCouArr:[] //输入的优惠券码的列表
             }
         },
         components: {lvvPopup,uniSegmentedControl},
@@ -375,37 +396,66 @@
 
             //获取默认门店信息
             this.getDefaultStore();
+						// #ifdef MP-ALIPAY || MP-TOUTIAO
+						let user_ship = this.$db.get('address_user_ship', true);
+						if (user_ship) {
+							this.userShip = user_ship;
+							this.params.area_id = user_ship.area_id;
+							this.$db.del('address_user_ship', true);
+						}
+						let user_invoice = this.$db.get('user_invoice', true);
+						if (user_invoice) {
+							this.invoice = user_invoice;
+							this.$db.del('user_invoice', true);
+						}
+						let user_store = this.$db.get('user_store', true);
+						if (user_store) {
+							this.store = user_store;
+							this.$db.del('user_store', true);
+						}
+						// #endif
+						// #ifdef H5 || APP-PLUS || APP-PLUS-NVUE
+						let user_ship = this.$store.state.userShip;
+						if (user_ship) {
+							this.userShip = user_ship;
+							this.params.area_id = user_ship.area_id;
+						}
+						let user_invoice = this.$store.state.invoice;
+						if (user_invoice) {
+							this.invoice = user_invoice;
+						}
+						// #endif
         },
 		onShow() {
-			// #ifdef MP-ALIPAY || MP-TOUTIAO
-			let user_ship = this.$db.get('address_user_ship', true);
-			if (user_ship) {
-				this.userShip = user_ship;
-				this.params.area_id = user_ship.area_id;
-				this.$db.del('address_user_ship', true);
-			}
-			let user_invoice = this.$db.get('user_invoice', true);
-			if (user_invoice) {
-				this.invoice = user_invoice;
-				this.$db.del('user_invoice', true);
-			}
-			let user_store = this.$db.get('user_store', true);
-			if (user_store) {
-				this.store = user_store;
-				this.$db.del('user_store', true);
-			}
-			// #endif
-			// #ifdef H5 || APP-PLUS || APP-PLUS-NVUE
-			let user_ship = this.$store.state.userShip;
-			if (user_ship) {
-				this.userShip = user_ship;
-				this.params.area_id = user_ship.area_id;
-			}
-			let user_invoice = this.$store.state.invoice;
-			if (user_invoice) {
-				this.invoice = user_invoice;
-			}
-			// #endif
+			// // #ifdef MP-ALIPAY || MP-TOUTIAO
+			// let user_ship = this.$db.get('address_user_ship', true);
+			// if (user_ship) {
+			// 	this.userShip = user_ship;
+			// 	this.params.area_id = user_ship.area_id;
+			// 	this.$db.del('address_user_ship', true);
+			// }
+			// let user_invoice = this.$db.get('user_invoice', true);
+			// if (user_invoice) {
+			// 	this.invoice = user_invoice;
+			// 	this.$db.del('user_invoice', true);
+			// }
+			// let user_store = this.$db.get('user_store', true);
+			// if (user_store) {
+			// 	this.store = user_store;
+			// 	this.$db.del('user_store', true);
+			// }
+			// // #endif
+			// // #ifdef H5 || APP-PLUS || APP-PLUS-NVUE
+			// let user_ship = this.$store.state.userShip;
+			// if (user_ship) {
+			// 	this.userShip = user_ship;
+			// 	this.params.area_id = user_ship.area_id;
+			// }
+			// let user_invoice = this.$store.state.invoice;
+			// if (user_invoice) {
+			// 	this.invoice = user_invoice;
+			// }
+			// // #endif
 		},
         methods: {
             // 切换门店
@@ -450,7 +500,6 @@
                 this.$api.cartList(data, res => {
                     if (res.status) {
                         let data = res.data
-
                         // 判断是否开启积分抵扣 并且 没有勾选积分使用
                         if (this.isOpenPoint === 1 && !this.isUsePoint) {
                             let money = {
@@ -494,9 +543,20 @@
                         this.promotions = data.promotion_list
                         // 使用的优惠券信息
                         this.usedCoupons = data.coupon
-
+												// 优惠券码
+												if(this.inputCouponCode){
+													let item={}
+													for(let key in this.usedCoupons){
+														item.code=key;
+														item.name=this.usedCoupons[key]
+													}
+													this.inputCouArr.push(item)
+												}
+												console.log(this.usedCoupons);
                         // 手动输入的优惠券使用成功后关闭弹窗并清除输入的优惠券码
-                        this.current === 1 && this.$refs.lvvpopref.popshow && this.inputCouponCode ? this.toclose() : ''
+                        // this.current === 1 && this.$refs.lvvpopref.popshow && this.inputCouponCode ? this.toclose() : ''
+                        this.current === 1 && this.$refs.lvvpopref.popshow
+												
                         this.inputCouponCode = ''
 
                         this.optCoupon = ''
@@ -508,9 +568,9 @@
                             // 优惠券已使用				15013
                             // 优惠券不符合使用规则		15014
                             // 优惠券不可使用多张			15015
-                            let errStatus = [15009, 15010, 15013, 15014, 15015]
-
-                            if (errStatus.indexOf(res.data) !== -1) {
+														//同一类优惠券只能用一张  11255
+                            let errStatus = [15009, 15010, 15013, 15014, 15015,11255]
+                            if (errStatus.indexOf(res.data.user_id) !== -1) { 
                                 // 删除使用的优惠券号码
                                 if (this.current === 1) {
                                     this.removeCouponCode(this.inputCouponCode, this.current)
@@ -556,7 +616,6 @@
 
                 // 暂存当次选中使用的优惠券key
                 this.optCoupon = this.userCoupons[index].coupon_code
-
                 let arr = []
                 this.userCoupons.forEach(item => {
                     if (item.checked) {
@@ -565,7 +624,10 @@
                 })
                 if (this.userCoupons[index].checked) {
                     // 使用
-                    this.params.coupon_code = arr.join()
+										this.params.coupon_code = arr.join()
+										// if(this.params.coupon_code){
+										// 	this.params.coupon_code += ','+arr.join()
+										// }
                 } else {
                     // 取消使用
                     let paramsCodes = this.params.coupon_code.split(',')
@@ -604,6 +666,13 @@
                 arr.splice(arr.indexOf(code), 1)
                 current === 0 ? this.optCoupon = '' : this.inputCouponCode = ''
                 this.params.coupon_code = arr.join()
+								let inputCouArr=[]
+								this.inputCouArr.forEach(item=>{
+									if(item.code!=code){
+										inputCouArr.push(item)
+									}
+								})
+								this.inputCouArr=inputCouArr
             },
             // 是否使用积分
             changePointHandle(){
@@ -1053,6 +1122,7 @@
 		border: 2upx solid #e8e8e8;
 		background-color: #fff;
 		height: 100%;
+			display: block;
 	}
 	.coupon-input input{
 		height: 100%;
@@ -1061,6 +1131,9 @@
 	}
 	.coupon-code{
 		margin: 4upx 30upx;
+	}
+	.input-arr{
+	
 	}
 	.coupon-enter-btn{
 		height: 100%;
@@ -1089,5 +1162,25 @@
 	.coupon-none-img{
 		width: 274upx;
 		height: 274upx;
+	}
+	.coupon-list{
+		padding: 0 40rpx;
+	}
+	.coupon-item{
+		background-color: #fff;
+		margin-bottom: 20rpx;
+		position: relative;
+	}
+	.coupon-name,.coupon-code1{
+		padding: 10rpx;
+	}
+	.delete{
+		position: absolute;
+		top: 0;
+		right: 0;
+	}
+	.delete image{
+		width: 40rpx;
+		height: 40rpx;
 	}
 </style>

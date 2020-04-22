6ff1643612b9141978e0474e80a9a3982670b157
wgg <1490100895@qq.com>
2020-04-22 11:40:14 +0800

    修改商品满赠喝订单满足字段

--- word/2020-04-21-商品满赠和订单满赠增加字段.sql
+++ word/2020-04-21-商品满赠和订单满赠增加字段.sql
@@ -1,5 +1,5 @@
-ALTER TABLE `order`
+ALTER TABLE `jshop_order`
 ADD COLUMN `giveaway` varchar(5000) NULL COMMENT '订单赠送商品信息' AFTER `coupon_pmt`;
 
-ALTER TABLE `order_items`
-ADD COLUMN `giveaway` varchar(5000) NULL COMMENT '商品赠品信息' AFTER `combination_goods_ids`;
+ALTER TABLE `jshop_order_items`
+ADD COLUMN `giveaway` varchar(5000) NULL COMMENT '商品赠品信息' AFTER `addon`;

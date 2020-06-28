055685bac9383de1a4b607eb303d9b0ead611d14
wgg <1490100895@qq.com>
2020-06-28 16:00:43 +0800

    库存权限

--- addons/StockControl/config.php
+++ addons/StockControl/config.php
@@ -1,103 +1,103 @@
 <?php
 return [
-    'menu' =>[
+    'menu' => [
         'title' => '菜单，此菜单就写到配置文件上吧，也不去可视化编辑了。',
         'value' => [
-            'menu_1' => [
+            [
                 'id' => 'stock_menu_1',
                 'parent_id' => '2',
                 'parent_menu_id' => '0',
                 'name' => '库存管理',
                 'type' => 'c',
-                'code' => 'index',          
+                'code' => 'index',
                 'perm_type' => 1,
                 'addons' => 'StockControl',
                 'sort' => 110
             ],
-            'menu_5' => [
+            [
                 'id' => 'stock_menu_5',
                 'parent_id' => 'stock_menu_1',
                 'parent_menu_id' => 'stock_menu_1',
                 'name' => '库存盘点',
                 'type' => 'a',
-                'code' => 'stockIndex',          
+                'code' => 'stockIndex',
                 'perm_type' => 1,
                 'addons' => 'StockControl',
                 'sort' => 110
             ],
-            'menu_5_1' => [
+            [
                 'id' => 'stock_menu_5_1',
-                'parent_id' => 'stock_menu_5',
+                'parent_id' => 'stock_menu_1',
                 'parent_menu_id' => 'stock_menu_5',
                 'name' => '修改库存',
                 'type' => 'a',
-                'code' => 'editStock',          
+                'code' => 'editStock',
                 'perm_type' => 2,
                 'addons' => 'StockControl',
                 'sort' => 110
             ],
-            'menu_2' => [
+            [
                 'id' => 'stock_menu_2',
                 'parent_id' => 'stock_menu_1',
                 'parent_menu_id' => 'stock_menu_1',
                 'name' => '入库管理',
                 'type' => 'a',
-                'code' => 'stock1Index',          
+                'code' => 'stock1Index',
                 'perm_type' => 1,
                 'addons' => 'StockControl',
                 'sort' => 110
             ],
-            'menu_2_1' => [
+            [
                 'id' => 'stock_menu_2_1',
-                'parent_id' => 'stock_menu_2',
+                'parent_id' => 'stock_menu_1',
                 'parent_menu_id' => 'stock_menu_2',
                 'name' => '新增入库单',
                 'type' => 'a',
-                'code' => 'stock1Add',          
+                'code' => 'stock1Add',
                 'perm_type' => 2,
                 'addons' => 'StockControl',
                 'sort' => 110
             ],
-            'menu_2_2' => [
+            [
                 'id' => 'stock_menu_2_2',
-                'parent_id' => 'stock_menu_2',
+                'parent_id' => 'stock_menu_1',
                 'parent_menu_id' => 'stock_menu_2',
                 'name' => '查看出入单详情',
                 'type' => 'a',
-                'code' => 'stockView',          
+                'code' => 'stockView',
                 'perm_type' => 2,
                 'addons' => 'StockControl',
                 'sort' => 110
             ],
-            'menu_3' => [
+            [
                 'id' => 'stock_menu_3',
                 'parent_id' => 'stock_menu_1',
                 'parent_menu_id' => 'stock_menu_1',
                 'name' => '出库管理',
                 'type' => 'a',
-                'code' => 'stock2Index',          
+                'code' => 'stock2Index',
                 'perm_type' => 1,
                 'addons' => 'StockControl',
                 'sort' => 110
             ],
-            'menu_3_1' => [
+            [
                 'id' => 'stock_menu_3_1',
-                'parent_id' => 'stock_menu_3',
+                'parent_id' => 'stock_menu_1',
                 'parent_menu_id' => 'stock_menu_3',
                 'name' => '新增出库单',
                 'type' => 'a',
-                'code' => 'stock2Add',          
+                'code' => 'stock2Add',
                 'perm_type' => 2,
                 'addons' => 'StockControl',
                 'sort' => 110
             ],
-            'menu_4' => [
+            [
                 'id' => 'stock_menu_4',
                 'parent_id' => 'stock_menu_1',
                 'parent_menu_id' => 'stock_menu_1',
                 'name' => '库存记录',
                 'type' => 'a',
-                'code' => 'index',          
+                'code' => 'index',
                 'perm_type' => 1,
                 'addons' => 'StockControl',
                 'sort' => 110

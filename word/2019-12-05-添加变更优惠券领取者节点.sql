ad7df278203c695aae1a9236a991bc9c8bcd903e
曾经最美 <hnhaitao@163.com>
2020-07-06 20:24:01 +0800

    移除早期的增量数据库包

--- word/2019-12-05-添加变更优惠券领取者节点.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `jshop_operation`(`id`, `parent_id`, `name`, `code`, `type`, `parent_menu_id`, `perm_type`, `sort`) VALUES (588, 263, '变更领取者', 'binduser', 'a', 361, 2, 100);
--- word/2020-03-09-用户日志增加类型.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `jshop_user_log`
-ADD COLUMN `type` tinyint(1) UNSIGNED NULL DEFAULT 1 COMMENT '类型，1会员，2管理员' AFTER `ip`;

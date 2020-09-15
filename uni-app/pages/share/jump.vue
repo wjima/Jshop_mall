2a2a96a31535d85f082986d7316737e064c1aaea
jingyi <1270411918@qq.com>
2020-09-15 14:36:25 +0800

    领取优惠券红包页面修改

--- uni-app/pages/share/jump.vue
+++ uni-app/pages/share/jump.vue
@@ -1,69 +1,71 @@
 <template>
 	<view class="container">
-		<view class="bg">
-			<image :src="`${url}static/images/welfarepro/bg_img.jpg`" mode="" class="img"></image>
+		<view class="bg" v-if="hasHb">
+			<image :src="`${url}static/images/welfarepro/bgi.png`" mode="" class="img"></image>
+			<!-- <image src="/static/image/welfarepro/bgi.png" mode="" class="img"></image> -->
 		</view>
-		<view class="red" v-if="hasHb">
+		<view class="red">
 			<view class="red_bgi">
-				<image :src="`${url}static/images/welfarepro/red.png`" mode="" class="img"></image>
+				<image :src="`${url}static/images/welfarepro/center.png`" mode="" class="img"></image>
+				<!-- <image src="/static/image/welfarepro/center.png" mode="" class="img"></image> -->
 			</view>
 			<view class="del" @click="goPage(1)">
-				<!-- <image src= apiBaseUrl+"/static/images/welfarepro/del_jump.png" mode="" class="del_icon"></image> -->
-				<image :src="`${url}static/images/welfarepro/del_jump.png`" mode="" class="del_icon"></image>
+				<image :src="`${url}static/images/welfarepro/del-jump.png`" mode="" class="del_icon"></image>
+				<!-- <image src="/static/image/welfarepro/del-jump.png" mode="" class="del_icon"></image> -->
+			</view>
+			<view class="logo">
+				<image :src='$store.state.config.shop_logo'></image>
 			</view>
 			<view class="word">
 				<view class="title">
-					恭喜您:
+					Jshop商城
 				</view>
 				<view class="content">
-					您有一个红包待领取
+					送您一个红包
 				</view>
 			</view>
 			<view class="btn" @click="receive(1)">
-				<!-- <image src=apiBaseUrl+"/static/images/welfarepro/red_btn.png" mode="" class="btn_icon"></image> -->
-				<image :src="`${url}static/images/welfarepro/red_btn.png`" mode="" class="btn_icon"></image>
-				<view class="btn_word">
-					立即领取
-				</view>
+				领取红包
 			</view>
 		</view>
 		<view class="red" v-if="hasCoupon">
 			<view class="red_bgi">
-				<!-- <image src=apiBaseUrl+"/static/images/welfarepro/coupon_bgi.png" mode="" class="img"></image> -->
-				<image :src="`${url}static/images/welfarepro/coupon_bgi.png`" mode="" class="img"></image>
+				<image :src="`${url}static/images/welfarepro/center.png`" mode="" class="img"></image>
+				<!-- <image src="/static/image/welfarepro/center.png" mode="" class="img"></image> -->
 			</view>
 			<view class="del" @click="goPage(2)">
-				<!-- <image src=apiBaseUrl+"/static/images/welfarepro/del_jump.png" mode="" class="del_icon"></image> -->
-				<image :src="`${url}static/images/welfarepro/del_jump.png`" mode="" class="del_icon"></image>
+				<image :src="`${url}static/images/welfarepro/del-jump.png`" mode="" class="del_icon"></image>
+				<!-- <image src="/static/image/welfarepro/del-jump.png" mode="" class="del_icon"></image> -->
 			</view>
-			<view class="word coupon">
+			<view class="logo">
+				<image :src='$store.state.config.shop_logo'></image>
+			</view>
+			<view class="word">
 				<view class="title">
-					恭喜您:
+					Jshop商城
 				</view>
 				<view class="content">
-					您有一个优惠券待领取
+					送您一张优惠券
 				</view>
 			</view>
-			<view class="btn coupon_btn" @click="receive(2)">
-				<!-- <image src=apiBaseUrl+"/static/images/welfarepro/coupon_btn.png" mode="" class="btn_icon"></image> -->
-				<image :src="`${url}static/images/welfarepro/coupon_btn.png`" mode="" class="btn_icon"></image>
-				<view class="btn_word">
-					立即领取
-				</view>
+			<view class="btn" @click="receive(2)">
+				领取优惠券
 			</view>
 		</view>
 	</view>
 </template>
 <script>
-	import { apiBaseUrl } from '@/config/config.js'	
+	import {
+		apiBaseUrl
+	} from '@/config/config.js'
 	export default {
 		data() {
 			return {
-				hasHb:false,
-				hasCoupon:false,
-				data:{},
-				welfarepro:["hb","coupon"],
-				url:''
+				hasHb: false,
+				hasCoupon: false,
+				data: {},
+				welfarepro: ["hb", "coupon"],
+				url: ''
 			};
 		},
 		onLoad(e) {
@@ -87,14 +89,14 @@
 					if (res.status) {
 						// console.log(res.data);
 						this.saveInviteCode(res.data.userShareCode); //存储邀请码
-						this.data=res.data;
-						if(res.data.welfarepro && res.data.welfarepro.length!=0){
-							if(res.data.welfarepro.indexOf('hb')>-1){
-								this.hasHb=true
-							}else if(res.data.welfarepro.indexOf('coupon')>-1){
-								this.hasCoupon=true
+						this.data = res.data;
+						if (res.data.welfarepro && res.data.welfarepro.length != 0) {
+							if (res.data.welfarepro.indexOf('hb') > -1) {
+								this.hasHb = true
+							} else if (res.data.welfarepro.indexOf('coupon') > -1) {
+								this.hasCoupon = true
 							}
-						}else{
+						} else {
 							this.goPage()
 						}
 						//           switch(res.data.page) {
@@ -142,94 +144,94 @@
 					}
 				});
 			},
-			
+
 			// 领取红包或者优惠券
-			receive(type){
-				let data={
-					userShareCode:this.data.userShareCode
+			receive(type) {
+				let data = {
+					userShareCode: this.data.userShareCode
 				}
-				if(type==1){
-					this.$api.getShareHb(data,res=>{
-						if(res.status){
-							this.$common.successToShow(res.msg,()=>{
-								this.hasHb=false
-								if(this.data.welfarepro.indexOf('coupon')>-1){
-									this.hasCoupon=true
-								}else{
+				if (type == 1) {
+					this.$api.getShareHb(data, res => {
+						if (res.status) {
+							this.$common.successToShow(res.msg, () => {
+								this.hasHb = false
+								if (this.data.welfarepro.indexOf('coupon') > -1) {
+									this.hasCoupon = true
+								} else {
 									this.goPage()
 								}
 							});
-						}else{
-							this.$common.errorToShow(res.msg,()=>{
-								this.hasHb=false
-								if(this.data.welfarepro.indexOf('coupon')>-1){
-									this.hasCoupon=true
-								}else{
+						} else {
+							this.$common.errorToShow(res.msg, () => {
+								this.hasHb = false
+								if (this.data.welfarepro.indexOf('coupon') > -1) {
+									this.hasCoupon = true
+								} else {
 									this.goPage()
 								}
 							})
 						}
 					})
-				}else if(type==2){
-					this.$api.getShareCoupon(data,res=>{
-						if(res.status){
-							this.$common.successToShow(res.msg,()=>{
-								this.hasCoupon=false
+				} else if (type == 2) {
+					this.$api.getShareCoupon(data, res => {
+						if (res.status) {
+							this.$common.successToShow(res.msg, () => {
+								this.hasCoupon = false
 								this.goPage()
 							});
-						}else{
-							this.$common.errorToShow(res.msg,()=>{
-								this.hasCoupon=false
+						} else {
+							this.$common.errorToShow(res.msg, () => {
+								this.hasCoupon = false
 								this.goPage()
 							})
 						}
 					})
 				}
-				
-				
+
+
 			},
 			//不领取的话跳转到分享的页面
-			goPage(type){
-				if(type==1){
-					if(this.data.welfarepro.indexOf('coupon')>-1){
-						this.hasCoupon=true
+			goPage(type) {
+				if (type == 1) {
+					if (this.data.welfarepro.indexOf('coupon') > -1) {
+						this.hasCoupon = true
 					}
-				}else{
+				} else {
 					console.log(this.data)
-					switch(this.data.page) {
+					switch (this.data.page) {
 						case '1': //首页
 							this.gotoIndex();
-								break;
+							break;
 						case '2': //商品
 							this.gotoGoods(this.data.params.goods_id);
-								break;
+							break;
 						case '3': //拼团
 							this.gotoPinTuan(this.data.params.goods_id);
-								break;
+							break;
 						case '4': //店铺邀请
 							this.gotoStore(this.data.params.store);
-								break;
+							break;
 						case '5': //文章页面
 							this.gotoArticle(this.data.params.article_id, this.data.params.article_type);
-								break;
+							break;
 						case '6': //参团页面
 							this.gotoInvitationGroup(this.data.params.goods_id, this.data.params.group_id, this.data.params.team_id)
-								break;
+							break;
 						case '7': //自定义页面
 							this.gotoCustom(this.data.params.page_code);
-								break;
+							break;
 						case '8': //智能表单
 							this.gotoForm(this.data.params.id)
-								break;
+							break;
 						case '9': //团购秒杀
 							this.gotoGroup(this.data.params.goods_id, this.data.params.group_id);
-								break;
-						case '10'://邀请砍价
+							break;
+						case '10': //邀请砍价
 							this.gotoBargain(this.data.params.id, this.data.params.type, this.data.params.record_id);
-								break;
-					  default:
-					    this.gotoIndex();
-								break;
+							break;
+						default:
+							this.gotoIndex();
+							break;
 					}
 				}
 			},
@@ -284,7 +286,7 @@
 			//跳转到参团
 			gotoInvitationGroup(id, group_id, team_id) {
 				if (id && id != '' && group_id && group_id != '' && team_id && team_id != '') {
-                    let url = '/pages/goods/index/pintuan?id=' + id + '&team_id=' + team_id;
+					let url = '/pages/goods/index/pintuan?id=' + id + '&team_id=' + team_id;
 					this.$common.redirectTo(url);
 				} else {
 					this.gotoIndex();
@@ -334,111 +336,105 @@
 	page {
 		width: 100%;
 		height: 100%;
-		// background: url("`${url}static/images/welfarepro/bg_img.jpg`") no-repeat;
-		// background-size: 100%;
 
 		.container {
 			position: relative;
 			width: 100%;
 			height: 100%;
-			.bg{
+
+			.bg {
 				width: 100%;
 				height: 100%;
+
 				// position: relative;
-				image{
+				image {
 					width: 100%;
 					height: 100%;
 				}
 			}
+
 			.red {
 				position: absolute;
-				top: 50%;
+				top: 45%;
 				left: 50%;
-				transform: translate(-50%,-50%);
-				width: 90%;
+				transform: translate(-50%, -50%);
+				width: 100%;
 				height: 70%;
+
 				.red_bgi {
 					width: 100%;
 					height: 100%;
+
 					.img {
 						width: 100%;
 						height: 100%;
 					}
 				}
-				.del{
+
+				.del {
 					position: absolute;
-					top: 30rpx;
-					right: 30rpx;
-					.del_icon{
+					top: 144rpx;
+					right: 82rpx;
+
+					.del_icon {
 						width: 60rpx;
 						height: 60rpx;
 					}
 				}
-				.word{
+
+				.logo {
 					position: absolute;
-					top: 50%;
+					top: 25%;
 					left: 50%;
-					transform: translate(-50%,-50%);
-					width: 80%;
-					.title{
-						font-size: 38rpx;
-						color: #dd991e;
+					transform: translateX(-50%);
+					width: 180rpx;
+					height: 180rpx;
+					border-radius: 50%;
+					background-color: #fff;
+
+					image {
+						position: absolute;
+						top: 50%;
+						left: 50%;
+						transform: translate(-50%, -50%);
+						width: 80%;
+						height: 80%;
+						border-radius: 50%;
 					}
-					.content{
-						font-size: 60rpx;
-						font-weight: 550;
-						color: #fcee50;
+				}
+
+				.word {
+					position: absolute;
+					left: 50%;
+					top: 47%;
+					transform: translateX(-50%);
+					color: #ffe3d6;
+
+					.title {
+						font-size: 38rpx;
+						text-align: center;
+					}
+
+					.content {
+						font-size: 50rpx;
 						margin-top: 30rpx;
 					}
 				}
-				.btn{
+
+				.btn {
 					position: absolute;
-					top: 75%;
-					left: 25%;
-					// transform: translateX(-50%);
-					width: 65%;
-					.btn_icon{
-						width: 100%;
-						height: 140rpx;
-					}
-					.btn_word{
-						font-size: 40rpx;
-						font-weight: 550;
-						color: #fcee50;
-						position: absolute;
-						top: 13%;
-						left: 22%;
-					}
-				}
-				.coupon{
-					top: 32%;
-					left: 62%;
-					.title{
-						color: #fff;
-					}
-					.content{
-						color: #fff;
-						font-size: 46rpx;
-					}
-				}
-				.coupon_btn{
-					top: 75%;
+					top: 95%;
 					left: 50%;
 					transform: translateX(-50%);
-					width: 50%;
-					.btn_icon{
-						width: 100%;
-						height: 100rpx;
-					}
-					.btn_word{
-						font-size: 40rpx;
-						font-weight: 550;
-						color: #fff;
-						position: absolute;
-						top: 45%;
-						left: 50%;
-						transform: translate(-50%,-50%);
-					}
+					width: 65%;
+					height: 100rpx;
+					line-height: 100rpx;
+					text-align: center;
+					border-radius: 10rpx;
+					font-size: 40rpx;
+					color: #fff;
+					background-color: #eb573e;
+					letter-spacing: 5rpx;
 				}
 			}
 		}

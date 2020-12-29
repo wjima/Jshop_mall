8b8b533c750184c4f267f1d3978cc35844b90fbf
Coco <1490100895@qq.com>
2020-12-29 20:41:47 +0800

    订单完成奖励积分时判断是否开启了积分功能

--- application/common/model/UserPointLog.php
+++ application/common/model/UserPointLog.php
@@ -346,10 +346,13 @@ class UserPointLog extends Common
      */
     public function orderComplete($user_id, $money, $order_id)
     {
-        $orders_reward_proportion = getSetting('orders_reward_proportion');
-        if ($orders_reward_proportion != 0) {
-            $point = floor($money / $orders_reward_proportion);
-            $this->setPoint($user_id, $point, self::POINT_TYPE_REBATE, '订单：' . $order_id . ' 积分奖励');
+        $point_switch = getSetting('point_switch');
+        if($point_switch == 1){
+            $orders_reward_proportion = getSetting('orders_reward_proportion');
+            if ($orders_reward_proportion != 0) {
+                $point = floor($money / $orders_reward_proportion);
+                $this->setPoint($user_id, $point, self::POINT_TYPE_REBATE, '订单：' . $order_id . ' 积分奖励');
+            }
         }
     }
 

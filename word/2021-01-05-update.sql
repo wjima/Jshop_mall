77bdca32ab4d277f3d2c85f4a5e8926d5c8d37d0
wjima <1113160783@qq.com>
2021-01-05 10:01:24 +0800

    会员表结构调整

--- word/2021-01-05-update.sql
+++ word/2021-01-05-update.sql
@@ -6,3 +6,9 @@ ALTER TABLE `jshop_images` CHANGE `name` `name` VARCHAR(500) CHARACTER SET utf8m
 
 ALTER TABLE `jshop_order`
 MODIFY COLUMN `promotion_list` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '优惠信息' AFTER `coupon`;
+
+ALTER TABLE `jshop_user`
+MODIFY COLUMN `balance` decimal(10, 2) NULL DEFAULT 0.00 COMMENT '余额' AFTER `nickname`,
+MODIFY COLUMN `point` int(11) UNSIGNED NULL DEFAULT 0 COMMENT '积分' AFTER `balance`,
+MODIFY COLUMN `grade` tinyint(2) UNSIGNED NULL DEFAULT 0 COMMENT '用户等级' AFTER `point`,
+MODIFY COLUMN `remarks` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '备注' AFTER `isdel`;

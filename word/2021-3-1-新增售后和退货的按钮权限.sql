6866dcdefa9fdf7e4ff15b7f388dea4cbf14604c
王海涛 <hnhaitao@163.com>
2021-05-11 14:41:39 +0800

    细节完善

--- word/2021-3-1-新增售后和退货的按钮权限.sql
+++ word/2021-3-1-新增售后和退货的按钮权限.sql
@@ -1,4 +1,4 @@
 -- 296   订单管理(Order)     297  订单列表(index)
-INSERT INTO `develop`.`jshop_operation`( `parent_id`, `name`, `code`, `type`, `parent_menu_id`, `perm_type`, `sort`) VALUES (296, '申请售后', 'aftersales', 'a', 297, 2, 100);
+INSERT INTO `jshop_operation`( `parent_id`, `name`, `code`, `type`, `parent_menu_id`, `perm_type`, `sort`) VALUES (296, '申请售后', 'aftersales', 'a', 297, 2, 100);
 -- 347 退货单管理(BillReship)    348 退货单列表(index)
-INSERT INTO `develop`.`jshop_operation`( `parent_id`, `name`, `code`, `type`, `parent_menu_id`, `perm_type`, `sort`) VALUES (347, '填写退货单', 'reship', 'a', 348, 2, 100);
+INSERT INTO `jshop_operation`( `parent_id`, `name`, `code`, `type`, `parent_menu_id`, `perm_type`, `sort`) VALUES (347, '填写退货单', 'reship', 'a', 348, 2, 100);

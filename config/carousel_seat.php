3e95463f4767ff66a69c41017ef0c06720c7214a
wjima <1457529125@qq.com>
2022-06-05 18:41:36 +0800

    增加默认广告位

--- config/carousel_seat.php
+++ config/carousel_seat.php
@@ -8,6 +8,7 @@
 return [
     'list' => [
         '首页幻灯片广告位' => 'tpl1_slider',
-        '分类页广告位'   => 'tpl1_class_banner1'
+        '分类页广告位'   => 'tpl1_class_banner1',
+        '文章顶部广告图'   => 'pc_article_top'
     ]
 ];
